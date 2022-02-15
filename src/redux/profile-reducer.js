3f0c884529f3b864158c4e40689b93211f2c94e6
Seremet Alexandru <andutzu.268@gmail.com>
2022-02-15 19:50:50 +0200

    details

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -4,7 +4,7 @@ const ADD_POST = 'ADD-POST';
 let defaultState = {
   posts: [
     { id: 1, text: 'Swallow my cum ;)', likes: 420 },
-    { id: 2, text: 'Futeo nahui', likes: 228 },
+    { id: 2, text: 'Hello', likes: 228 },
     { id: 3, text: 'Welcome to the club, buddy.', likes: 69 },
   ],
   storedText: '',
