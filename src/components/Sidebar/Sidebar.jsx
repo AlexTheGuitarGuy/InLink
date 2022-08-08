ee87ece10eaa90aeb3da2f68bd6a31a2119afc18
Seremet Alexandru <andutzu.268@gmail.com>
2022-08-09 02:53:34 +0300

    wrapped bad setState in useEffect

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,26 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
 import FriendItems from './FriendItems/FriendItems';
 import { compose } from 'redux';
 import { useLocation } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setIsSidebarHidden } from '../../redux/app-reducer';
+import { getIsSidebarHidden } from '../../redux/app-selector';
 import useScreenSize from '../../hooks/useScreenSize';
 
 const Sidebar = () => {
   const dispatch = useDispatch();
   const screenSize = useScreenSize();
+  const location = useLocation();
 
-  if (useLocation().pathname.match('/messages') || screenSize.dynamicWidth < 1366) {
-    dispatch(setIsSidebarHidden(true));
-    return null;
-  } else dispatch(setIsSidebarHidden(false));
+  const isSidebarHidden = useSelector(getIsSidebarHidden);
+
+  useEffect(() => {
+    if (location.pathname.match('/messages') || screenSize.dynamicWidth < 1366) {
+      dispatch(setIsSidebarHidden(true));
+    } else dispatch(setIsSidebarHidden(false));
+  }, [dispatch, location.pathname]);
+
+  if (isSidebarHidden) return null;
 
   return (
     <div className="p-6">
